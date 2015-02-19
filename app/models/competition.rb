7571a834394112fb18416a711a1e523a86110af4
atiu94 <adrian.tiu@obf.ateneo.edu>
2015-02-19 17:45:40 +0800

    Added validation

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -1,2 +1,10 @@
 class Competition < ActiveRecord::Base
+
+	validates :name, presence :true, length :{maximum: 10}
+	validates :name, presence :true
+	validates :arqp_contingent_debater, presence :true
+	validates :arqp_contingent_adjudicator, presence :true
+	validates :arqp_non_contingent, presence :true
+	validates :presidential_approval_status, presence :true
+	validates :quota_point_monetary_value, :presence :true
 end
